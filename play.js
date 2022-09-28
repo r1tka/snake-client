82799006fb663478f36f0102fe17470b0ddaa3a9
Margaryta <margaryta.lanova@gmail.com>
2022-09-28 13:41:33 -0700

    connecting to a server and handle messages from server

---
+++ play.js
@@ -0,0 +1,23 @@
+// this will serve my main file through which I will launch th game client
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541
+  });
+  conn.on("connect", () => {
+    console.log("Connected")
+  })
+  conn.on("data", (data) => {
+    console.log("Data:", data)
+  })
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
