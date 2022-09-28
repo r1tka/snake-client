9e9164363dc8dc31b43f223a039772f45d6b0320
Margaryta <margaryta.lanova@gmail.com>
2022-09-28 15:07:58 -0700

    handling CTRL + C User input

--- play.js
+++ play.js
@@ -11,8 +11,27 @@ const connect = function () {
     console.log("Successfully connected to game server")
     conn.write("Name: ML");  
   })
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+const handleUserInput = function (data) {
+  console.log("DATA", data)
+  if (data === '\u0003') {
+    process.exit();
+    // conn.write("Move: up"); 
+
+  }
+
+  // your code here
+};
   // conn.on("connect", () => {
-  //   conn.write("Move: up");  
   // })
 
 
@@ -22,7 +41,7 @@ const connect = function () {
   
   // interpret incoming data as text
   conn.setEncoding("utf8");
-
+  setupInput();
   return conn;
 };
 
